060f5e4799cc2f616aa3a5ff6b6c4b9d5943ca67
ivandalechenko <ivandalechenko@gmail.com>
2025-01-04 19:34:47 +0200

    add deploy and checl check**

--- components/tweets/tweets-router.js
+++ components/tweets/tweets-router.js
@@ -4,5 +4,6 @@ const router = new Router();
 
 // router.post('/', tweetsController.set); 
 router.get('/', tweetsController.get);
+router.get('/check', (req, res) => res.json('ok'));
 
 module.exports = router;
